58cbf0ff58f9214f94b063425abe2eba14416946
Ulises <unh248@gmail.com>
2023-08-12 10:48:29 -0600

    first commmit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("showRosesButton").addEventListener("click", function() {
+  const roseContainer = document.getElementById("roseContainer");
+  const body = document.body;
+  const audio = new Audio("Turning page.mp3"); // Cambia "Turning page.mp3" por la ruta de tu canción
+
+  roseContainer.innerHTML = "<img src='rosas.png' alt='Ramo de Rosas'>";
+  roseContainer.style.display = "block";
+  audio.play();
+  body.style.backgroundImage = "url('estrellas.gif')"; // Cambiar al nuevo fondo con estrellas
+});
